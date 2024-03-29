56f4adf45e80b07e25ec963cd64843ff1d2c1e1b
DouglasGuacaran <douglasguacaran@gmail.com>
2024-03-29 17:07:56 -0300

    Cambios Douglas

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,15 +1,38 @@
-import { createContext } from "react";
+import React, { createContext, useContext, useReducer, useMemo } from "react";
 
-export const initialState = {theme: "", data: []}
+export const initialState = { theme: "light", data: [] };
 
-export const ContextGlobal = createContext(undefined);
+// Acciones
+export const actionTypes = {
+  TOGGLE_THEME: 'TOGGLE_THEME',
+};
+
+// Reductor
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.TOGGLE_THEME:
+      return {
+        ...state,
+        theme: state.theme === "light" ? "dark" : "light",
+      };
+    default:
+      return state;
+  }
+};
+
+// Contexto Global
+const ContextGlobal = createContext(initialState);
 
 export const ContextProvider = ({ children }) => {
-  //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = useMemo(() => ({ state, dispatch }), [state, dispatch]);
 
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={value}>
       {children}
     </ContextGlobal.Provider>
   );
 };
+
+// Hook para usar el contexto global
+export const useContextGlobal = () => useContext(ContextGlobal);
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,16 +2,19 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Card from '../Components/Card'
 import { useDentistStates } from '../Context';
+import { useContextGlobal } from '../Components/utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
-  const{state} = useDentistStates();
+  
+  const { state: dentistState } = useDentistStates();
+  const { state: globalState } = useContextGlobal();
   // console.log(state);
 
-
+  const themeClass = state.theme === 'dark' ? 'dark' : 'light';
   return (
-    <main className="" >
+    <main className={`contact ${themeClass}`} >
       <h1>CLINICA ODONTOLOGICA</h1>
       <div className='card-grid'>
         {state.list.map((dentista) => <Card key={dentista.id} item={dentista}/>)}
